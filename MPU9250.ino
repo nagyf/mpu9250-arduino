146640b52ec1fd6884bcff8fdeb4e27833ff9be7
Ferenc Nagy <nagy.ferenc.jr@gmail.com>
2018-10-09 20:23:55 +0200

    Added a 2sec startup into setup function

--- MPU9250.ino
+++ MPU9250.ino
@@ -47,9 +47,10 @@ void setup() {
   // setting the gyroscope full scale range to +/-500 deg/s
   IMU.setGyroRange(MPU9250::GYRO_RANGE_500DPS);
   // setting DLPF bandwidth to 20 Hz
-  IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
+  //IMU.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_20HZ);
   // setting SRD to 19 for a 50 Hz update rate
-  IMU.setSrd(19);
+  //IMU.setSrd(19);
+  delay(2000);
 }
 
 void loop() {
