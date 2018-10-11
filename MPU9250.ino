62dc08e7361f90afecda52124cabd94ca493f61f
Ferenc Nagy <nagy.ferenc.jr@gmail.com>
2018-10-11 13:11:14 +0200

    Added Yaw / Pitch / Roll

--- MPU9250.ino
+++ MPU9250.ino
@@ -1,32 +1,17 @@
-/*
-  Basic_I2C.ino
-  Brian R Taylor
-  brian.taylor@bolderflight.com
-
-  Copyright (c) 2017 Bolder Flight Systems
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-  and associated documentation files (the "Software"), to deal in the Software without restriction,
-  including without limitation the rights to use, copy, modify, merge, publish, distribute,
-  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all copies or
-  substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 #include "MPU9250.h"
+
+#define Kp 2.0f * 5.0f // these are the free parameters in the Mahony filter and fusion scheme, Kp for proportional feedback, Ki for integral
+#define Ki 0.0f
 
 // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
 MPU9250 IMU(Wire, 0x68);
 int status;
-
+float deltat = 0.0f;
+float eInt[3] = {0.0f, 0.0f, 0.0f};
+float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};
+uint32_t lastUpdate = 0, firstUpdate = 0; // used to calculate integration interval
+uint32_t now = 0;
+ 
 void setup() {
   // serial to display data
   Serial.begin(115200);
@@ -56,25 +41,152 @@ void setup() {
 void loop() {
   // read the sensor
   IMU.readSensor();
-  // display the data
-  Serial.print(IMU.getAccelX_mss(), 6);
+  now = micros();
+  deltat = ((now - lastUpdate)/1000000.0f); // set integration time by time elapsed since last filter update
+  lastUpdate = now;
+
+  float ax = IMU.getAccelX_mss();
+  float ay = IMU.getAccelY_mss();
+  float az = IMU.getAccelZ_mss();
+  
+  float gx = IMU.getGyroX_rads();
+  float gy = IMU.getGyroX_rads();
+  float gz = IMU.getGyroX_rads();
+
+  float mx = IMU.getMagX_uT();
+  float my = IMU.getMagY_uT();
+  float mz = IMU.getMagZ_uT();
+
+  MahonyQuaternionUpdate(ax, ay, az, gx, gy, gz, my, mx, mz);
+
+  float yaw   = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);   
+  float pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
+  float roll  = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
+  pitch *= 180.0f / PI;
+  yaw   *= 180.0f / PI; 
+  yaw   -= 13.8; // Declination at Danville, California is 13 degrees 48 minutes and 47 seconds on 2014-04-04
+  roll *= 180.0f / PI;
+
+  Serial.print(ax, 6);
   Serial.print(";");
-  Serial.print(IMU.getAccelY_mss(), 6);
+  Serial.print(ay, 6);
   Serial.print(";");
-  Serial.print(IMU.getAccelZ_mss(), 6);
+  Serial.print(az, 6);
   Serial.print(";");
-  Serial.print(IMU.getGyroX_rads(), 6);
+  
+  Serial.print(gx, 6);
   Serial.print(";");
-  Serial.print(IMU.getGyroY_rads(), 6);
+  Serial.print(gy, 6);
   Serial.print(";");
-  Serial.print(IMU.getGyroZ_rads(), 6);
+  Serial.print(gz, 6);
   Serial.print(";");
-  Serial.print(IMU.getMagX_uT(), 6);
+  
+  Serial.print(mx, 6);
   Serial.print(";");
-  Serial.print(IMU.getMagY_uT(), 6);
+  Serial.print(my, 6);
   Serial.print(";");
-  Serial.print(IMU.getMagZ_uT(), 6);
+  Serial.print(mz, 6);
   Serial.print(";");
+
+  Serial.print(yaw, 6);
+  Serial.print(";");
+  Serial.print(pitch, 6);
+  Serial.print(";");
+  Serial.print(roll, 6);
+  Serial.print(";");
+  
   Serial.println(IMU.getTemperature_C(), 6);
   delay(100);
+}
+
+// Similar to Madgwick scheme but uses proportional and integral filtering on the error between estimated reference vectors and measured ones
+void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz) {
+  float q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];   // short name local variable for readability
+  float norm;
+  float hx, hy, bx, bz;
+  float vx, vy, vz, wx, wy, wz;
+  float ex, ey, ez;
+  float pa, pb, pc;
+
+  // Auxiliary variables to avoid repeated arithmetic
+  float q1q1 = q1 * q1;
+  float q1q2 = q1 * q2;
+  float q1q3 = q1 * q3;
+  float q1q4 = q1 * q4;
+  float q2q2 = q2 * q2;
+  float q2q3 = q2 * q3;
+  float q2q4 = q2 * q4;
+  float q3q3 = q3 * q3;
+  float q3q4 = q3 * q4;
+  float q4q4 = q4 * q4;   
+
+  // Normalise accelerometer measurement
+  norm = sqrtf(ax * ax + ay * ay + az * az);
+  if (norm == 0.0f) return; // handle NaN
+  norm = 1.0f / norm;        // use reciprocal for division
+  ax *= norm;
+  ay *= norm;
+  az *= norm;
+
+  // Normalise magnetometer measurement
+  norm = sqrtf(mx * mx + my * my + mz * mz);
+  if (norm == 0.0f) return; // handle NaN
+  norm = 1.0f / norm;        // use reciprocal for division
+  mx *= norm;
+  my *= norm;
+  mz *= norm;
+
+  // Reference direction of Earth's magnetic field
+  hx = 2.0f * mx * (0.5f - q3q3 - q4q4) + 2.0f * my * (q2q3 - q1q4) + 2.0f * mz * (q2q4 + q1q3);
+  hy = 2.0f * mx * (q2q3 + q1q4) + 2.0f * my * (0.5f - q2q2 - q4q4) + 2.0f * mz * (q3q4 - q1q2);
+  bx = sqrtf((hx * hx) + (hy * hy));
+  bz = 2.0f * mx * (q2q4 - q1q3) + 2.0f * my * (q3q4 + q1q2) + 2.0f * mz * (0.5f - q2q2 - q3q3);
+
+  // Estimated direction of gravity and magnetic field
+  vx = 2.0f * (q2q4 - q1q3);
+  vy = 2.0f * (q1q2 + q3q4);
+  vz = q1q1 - q2q2 - q3q3 + q4q4;
+  wx = 2.0f * bx * (0.5f - q3q3 - q4q4) + 2.0f * bz * (q2q4 - q1q3);
+  wy = 2.0f * bx * (q2q3 - q1q4) + 2.0f * bz * (q1q2 + q3q4);
+  wz = 2.0f * bx * (q1q3 + q2q4) + 2.0f * bz * (0.5f - q2q2 - q3q3);  
+
+  // Error is cross product between estimated direction and measured direction of gravity
+  ex = (ay * vz - az * vy) + (my * wz - mz * wy);
+  ey = (az * vx - ax * vz) + (mz * wx - mx * wz);
+  ez = (ax * vy - ay * vx) + (mx * wy - my * wx);
+  if (Ki > 0.0f)
+  {
+      eInt[0] += ex;      // accumulate integral error
+      eInt[1] += ey;
+      eInt[2] += ez;
+  }
+  else
+  {
+      eInt[0] = 0.0f;     // prevent integral wind up
+      eInt[1] = 0.0f;
+      eInt[2] = 0.0f;
+  }
+
+  // Apply feedback terms
+  gx = gx + Kp * ex + Ki * eInt[0];
+  gy = gy + Kp * ey + Ki * eInt[1];
+  gz = gz + Kp * ez + Ki * eInt[2];
+
+  // Integrate rate of change of quaternion
+  pa = q2;
+  pb = q3;
+  pc = q4;
+  q1 = q1 + (-q2 * gx - q3 * gy - q4 * gz) * (0.5f * deltat);
+  q2 = pa + (q1 * gx + pb * gz - pc * gy) * (0.5f * deltat);
+  q3 = pb + (q1 * gy - pa * gz + pc * gx) * (0.5f * deltat);
+  q4 = pc + (q1 * gz + pa * gy - pb * gx) * (0.5f * deltat);
+
+  // Normalise quaternion
+  norm = sqrtf(q1 * q1 + q2 * q2 + q3 * q3 + q4 * q4);
+  norm = 1.0f / norm;
+  q[0] = q1 * norm;
+  q[1] = q2 * norm;
+  q[2] = q3 * norm;
+  q[3] = q4 * norm;
+ 
 }
